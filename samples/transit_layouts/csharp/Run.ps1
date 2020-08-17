b066a0f3131d9227a4e1f790abd2fc8d15436172
Per Rovegård <per@rovegard.se>
2020-08-17 07:43:37 +0200

    Add Run.ps1 helper

---
+++ samples/transit_layouts/csharp/Run.ps1
@@ -0,0 +1,15 @@
+param(
+    [Parameter(Mandatory=$true)]
+    [string]$name,
+
+    [Parameter(Mandatory=$true)]
+    [string]$projectId,
+
+    [Parameter(Mandatory=$true)]
+    [string]$projectApiKey,
+
+    [parameter(Mandatory=$false)]
+    [string[]]$transitIds
+)
+
+dotnet run -p "${name}.csproj" $projectId $projectApiKey (,$transitIds)
